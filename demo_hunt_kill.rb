31270e392766f4abad19c136518107402eb5685e
samskinnerphd <samskinnerphd@gmail.com>
2017-02-11 11:56:49 -0500

    demo hunt & kill

---
+++ demo_hunt_kill.rb
@@ -0,0 +1,9 @@
+require 'grid'
+require 'hunt_and_kill'
+
+grid = Grid.new(20,20)
+HuntAndKill.on(grid)
+puts "here"
+filename = 'images/huntandkill.png'
+grid.to_png.save(filename)
+`open #{filename}`
