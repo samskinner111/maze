65a65bf625d3d7eb168bd15b3119f79153b25c76
samskinnerphd <samskinnerphd@gmail.com>
2017-02-12 11:08:45 -0500

    add Recursive Backtracker demo

---
+++ demo_recursive_backtracker.rb
@@ -0,0 +1,9 @@
+require 'grid'
+require 'recursive_backtracker'
+
+grid = Grid.new(20,20)
+RecursiveBacktracker.on(grid)
+
+filename = 'images/recursive.png'
+grid.to_png.save(filename)
+`open #{filename}`
