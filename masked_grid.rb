791481c9eadf8a9c2050315601444ceb7b269d31
samskinnerphd <samskinnerphd@gmail.com>
2017-02-13 23:14:31 -0500

    first mask

---
+++ masked_grid.rb
@@ -0,0 +1,28 @@
+require 'grid'
+
+class MaskedGrid < Grid
+  attr_reader :mask
+
+  def initialize(mask)
+  	@mask = mask
+  	super(@mask.rows, @mask.columns)
+  end
+
+  def prepare_grid
+  	Array.new(rows) do |row|
+  		Array.new(columns) do |column|
+  			Cell.new(row, column) if @mask[row][column]
+  		end
+  	end
+  end
+
+  def random_cell
+  	row, col = @mask.random_location
+  	sefl[row, col]
+  end
+
+  def size
+  	@mask.count
+  end
+  
+end
