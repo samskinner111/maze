a0df7989676aed6e688b7a584786f0a06237e959
samskinnerphd <samskinnerphd@gmail.com>
2017-02-17 17:43:58 -0500

    add hex cell class

---
+++ hex_cell.rb
@@ -0,0 +1,19 @@
+require 'cell'
+
+class HexCell < Cell
+
+  attr_accessor :notheast, :northwest
+  attr_accessor :southeast, :southwest
+
+  def neighbors
+    list = []
+    list << northwest if northwest
+    list << north if north
+    list << northeast if northeast
+    list << southwest if southwest
+    list << south if south
+    list << southeast if southeast
+    list 
+  end
+  
+end
