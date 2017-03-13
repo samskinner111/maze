8a2775f2ca4a0ecd1176bbd944e6b74de247e332
samskinnerphd <samskinnerphd@gmail.com>
2017-03-13 07:18:20 -0400

    true Prim's algorithm

--- demo_prims.rb
+++ demo_prims.rb
@@ -4,6 +4,14 @@ require 'grid'
 grid = Grid.new(20, 20)
 SimplifiedPrims.on(grid)
 
-filename = 'images/prims.png'
+filename = 'images/simple_prims.png'
+grid.to_png.save(filename)
+`open #{filename}`
+
+
+grid = Grid.new(20, 20)
+TruePrims.on(grid)
+
+filename = 'images/true_prims.png'
 grid.to_png.save(filename)
 `open #{filename}`
--- prims.rb
+++ prims.rb
@@ -17,5 +17,32 @@ class SimplifiedPrims
       end
     end
     grid
+  end 
+end
+
+
+class TruePrims
+
+  def self.on(grid, start_at: grid.random_cell)
+    active = []
+    active.push(start_at)
+
+    costs = {}
+    grid.each_cell { |cell| costs[cell] = rand(100) }
+
+    while active.any?
+    # refactor using a priority queue rather than Array.min
+    cell = active.min { |a,b| costs[a] <=> costs[b] }
+    available_neighbors = cell.neighbors.select { |n| n.links.empty? }
+
+      if available_neighbors.any?
+        neighbor = available_neighbors.min { |a,b| costs[a] <=> costs[b] }
+        cell.link(neighbor)
+        active.push(neighbor)
+      else
+        active.delete(cell)
+      end
+    end
+    grid
   end
 end
