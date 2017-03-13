1852ecd361ee8b4d4b9196668240bbcffb23852a
samskinnerphd <samskinnerphd@gmail.com>
2017-03-13 07:44:47 -0400

    Growing Tree algorithm

---
+++ demo_growing_tree.rb
@@ -0,0 +1,19 @@
+require 'growing_tree'
+require 'grid'
+
+def save(grid, filename)
+  grid.to_png.save(filename)
+  `open #{filename}`
+end
+
+grid = Grid.new(20, 20)
+GrowingTree.on(grid) { |list| list.sample }
+save(grid, "growing_tree_random.png")
+
+grid = Grid.new(20, 20)
+GrowingTree.on(grid) { |list| list.last }
+save(grid, "growing_tree_last.png")
+
+grid = Grid.new(20, 20)
+GrowingTree.on(grid) { |list| (rand(2) == 0) ? list.last : list.sample }
+save(grid, "growing_tree_mix.png")
